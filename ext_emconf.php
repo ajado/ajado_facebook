0328624839347549ffbf7c21ae1f37ef90a0bd82
ajado web applications & content management <msavio@ajado.com>
2011-12-16 10:08:39 +0100

    Manual, first initial upload

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ajado_facebook".
 #
-# Auto generated 13-10-2011 16:38
+# Auto generated 15-12-2011 14:54
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.0',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -39,6 +39,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"41db";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"eb95";s:14:"ext_tables.php";s:4:"b7df";s:14:"ext_tables.sql";s:4:"cfe8";s:16:"locallang_db.xml";s:4:"5e7c";s:14:"doc/manual.odt";s:4:"646b";s:19:"doc/wizard_form.dat";s:4:"82bc";s:20:"doc/wizard_form.html";s:4:"9f3f";s:21:"lib/base_facebook.php";s:4:"1f1b";s:16:"lib/channel.html";s:4:"c7d9";s:16:"lib/facebook.php";s:4:"9ca9";s:34:"pi1/class.tx_ajadofacebook_pi1.php";s:4:"d226";s:17:"pi1/locallang.xml";s:4:"10c3";s:43:"static/ajado_facebook_connect/constants.txt";s:4:"d41d";s:39:"static/ajado_facebook_connect/setup.txt";s:4:"d41d";s:37:"static/facebook_connect/constants.txt";s:4:"6bfd";s:33:"static/facebook_connect/setup.txt";s:4:"7dd3";}',
 );
 
 ?>
