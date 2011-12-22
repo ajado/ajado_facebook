fcb09e94844772e7038f9ab73793f466f2bd8707
ajado web applications & content management <msavio@ajado.com>
2011-12-22 15:44:13 +0100

    Upload v.0.2.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ajado_facebook".
 #
-# Auto generated 20-12-2011 13:03
+# Auto generated 22-12-2011 15:43
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'ajado.com',
-	'version' => '0.2.0',
+	'version' => '0.2.1',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"41db";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"356a";s:17:"ext_localconf.php";s:4:"eb95";s:14:"ext_tables.php";s:4:"b7df";s:14:"ext_tables.sql";s:4:"cfe8";s:16:"locallang_db.xml";s:4:"5e7c";s:14:"doc/manual.odt";s:4:"e860";s:14:"doc/manual.sxw";s:4:"98b4";s:19:"doc/wizard_form.dat";s:4:"82bc";s:20:"doc/wizard_form.html";s:4:"9f3f";s:21:"lib/base_facebook.php";s:4:"1f1b";s:16:"lib/channel.html";s:4:"c7d9";s:16:"lib/facebook.php";s:4:"9ca9";s:34:"pi1/class.tx_ajadofacebook_pi1.php";s:4:"d226";s:17:"pi1/locallang.xml";s:4:"10c3";s:43:"static/ajado_facebook_connect/constants.txt";s:4:"d41d";s:39:"static/ajado_facebook_connect/setup.txt";s:4:"d41d";s:37:"static/facebook_connect/constants.txt";s:4:"966e";s:33:"static/facebook_connect/setup.txt";s:4:"5968";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"41db";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"356a";s:17:"ext_localconf.php";s:4:"eb95";s:14:"ext_tables.php";s:4:"b7df";s:14:"ext_tables.sql";s:4:"cfe8";s:16:"locallang_db.xml";s:4:"5e7c";s:14:"doc/manual.odt";s:4:"f04a";s:14:"doc/manual.sxw";s:4:"98b4";s:19:"doc/wizard_form.dat";s:4:"82bc";s:20:"doc/wizard_form.html";s:4:"9f3f";s:21:"lib/base_facebook.php";s:4:"1f1b";s:16:"lib/channel.html";s:4:"c7d9";s:16:"lib/facebook.php";s:4:"9ca9";s:34:"pi1/class.tx_ajadofacebook_pi1.php";s:4:"d226";s:17:"pi1/locallang.xml";s:4:"10c3";s:43:"static/ajado_facebook_connect/constants.txt";s:4:"d41d";s:39:"static/ajado_facebook_connect/setup.txt";s:4:"d41d";s:37:"static/facebook_connect/constants.txt";s:4:"966e";s:33:"static/facebook_connect/setup.txt";s:4:"5968";}',
 );
 
 ?>
