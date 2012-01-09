3f8a22ceaf7314c01bf7a62c1ebcd370345220c5
ajado web applications & content management <msavio@ajado.com>
2012-01-09 16:13:53 +0100

    TER Upload v0.3.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ajado_facebook".
 #
-# Auto generated 08-01-2012 00:30
+# Auto generated 09-01-2012 16:13
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Facebook Connect via OAuth 2.0, works together with sr_feuser_register. Introduction: http://www.ajado.com/blog/presentation-facebook-connect/',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.3.1',
+	'version' => '0.3.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"41db";s:10:"README.txt";s:4:"ee2d";s:20:"class.ext_update.php";s:4:"aea7";s:12:"ext_icon.gif";s:4:"356a";s:17:"ext_localconf.php";s:4:"eb95";s:14:"ext_tables.php";s:4:"a245";s:14:"ext_tables.sql";s:4:"0acf";s:16:"locallang_db.xml";s:4:"5e7c";s:14:"doc/manual.odt";s:4:"0dd8";s:14:"doc/manual.sxw";s:4:"4220";s:21:"lib/base_facebook.php";s:4:"1f1b";s:16:"lib/channel.html";s:4:"c7d9";s:16:"lib/facebook.php";s:4:"9ca9";s:34:"pi1/class.tx_ajadofacebook_pi1.php";s:4:"e268";s:17:"pi1/locallang.xml";s:4:"10c3";s:37:"static/facebook_connect/constants.txt";s:4:"966e";s:33:"static/facebook_connect/setup.txt";s:4:"985a";}',
+	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"41db";s:10:"README.txt";s:4:"ee2d";s:20:"class.ext_update.php";s:4:"aea7";s:12:"ext_icon.gif";s:4:"356a";s:17:"ext_localconf.php";s:4:"eb95";s:14:"ext_tables.php";s:4:"a245";s:14:"ext_tables.sql";s:4:"0acf";s:16:"locallang_db.xml";s:4:"5e7c";s:14:"doc/manual.odt";s:4:"0dd8";s:14:"doc/manual.sxw";s:4:"4220";s:21:"lib/base_facebook.php";s:4:"1f1b";s:16:"lib/channel.html";s:4:"c7d9";s:16:"lib/facebook.php";s:4:"9ca9";s:34:"pi1/class.tx_ajadofacebook_pi1.php";s:4:"1037";s:17:"pi1/locallang.xml";s:4:"10c3";s:37:"static/facebook_connect/constants.txt";s:4:"966e";s:33:"static/facebook_connect/setup.txt";s:4:"985a";}',
 	'suggests' => array(
 	),
 );
